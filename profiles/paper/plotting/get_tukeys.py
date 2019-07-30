909305b9656fdb597dab698f4748a070b75c892a
Sarah Appleby <sarahappleby20@gmail.com>
2019-07-30 10:45:48 +0000

    get satellites and centrals for >10.5

--- profiles/paper/plotting/get_tukeys.py
+++ profiles/paper/plotting/get_tukeys.py
@@ -1,12 +1,7 @@
 import h5py
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 from plotting_methods import *
-
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rcParams.update({'font.size': 14})
 
 model = sys.argv[1]
 wind = sys.argv[2]
@@ -202,4 +197,126 @@ for i, m in enumerate(masks):
         f.create_dataset('all_small_scale_'+bin_labels[i], data=np.array(all_fmol_small_scale[i]))
         f.create_dataset('cen_small_scale_'+bin_labels[i], data=np.array(cen_fmol_small_scale[i]))
         f.create_dataset('sat_small_scale_'+bin_labels[i], data=np.array(sat_fmol_small_scale[i]))
+
+# for gals > 10.5:
+bin_label = '>10.5'
+
+with h5py.File(centrals_dir+'mask_3_all_profiles.h5', 'r') as f:
+    cen_star_m = f['sm'].value
+    cen_gas_sfr = f['gas_sfr'].value
+    cen_gas_h1 = f['h1_m'].value
+    cen_gas_h2 = f['h2_m'].value
+
+with h5py.File(centrals_dir+'mask_4_all_profiles.h5', 'r') as f:
+    cen_star_m = np.concatenate((cen_star_m, f['sm'].value))
+    cen_gas_sfr = np.concatenate((cen_gas_sfr, f['gas_sfr'].value))
+    cen_gas_h1 = np.concatenate((cen_gas_h1,f['h1_m'].value))
+    cen_gas_h2 = np.concatenate((cen_gas_h2,f['h2_m'].value))
+
+with h5py.File(sats_dir+'mask_3_all_profiles.h5', 'r') as f:
+    sat_star_m = f['sm'].value
+    sat_gas_sfr = f['gas_sfr'].value
+    sat_gas_h1 = f['h1_m'].value
+    sat_gas_h2 = f['h2_m'].value
+
+with h5py.File(sats_dir+'mask_4_all_profiles.h5', 'r') as f:
+    sat_star_m = np.concatenate((sat_star_m, f['sm'].value))
+    sat_gas_sfr = np.concatenate((sat_gas_sfr, f['gas_sfr'].value))
+    sat_gas_h1 = np.concatenate((sat_gas_h1, f['h1_m'].value))
+    sat_gas_h2 = np.concatenate((sat_gas_h2, f['h2_m'].value))
+
+# centrals:
+cen_no_gals = len(cen_gas_sfr)
+
+tukey, scale = tukey_biweight(cen_gas_sfr)
+tukey[np.where(tukey == 0.)[0]] = 1.e-6
+cen_sfr_tukey = np.log10(tukey)
+cen_sfr_large_scale = scale / (np.log(10.)*tukey)
+cen_sfr_small_scale = scale / (np.sqrt(cen_no_gals)* np.log(10.)*tukey)
+
+cen_gas_ssfr = cen_gas_sfr / cen_star_m
+tukey, scale = tukey_biweight(cen_gas_ssfr)
+cen_ssfr_tukey = np.log10(tukey)
+cen_ssfr_large_scale = scale / (np.log(10.)*tukey)
+cen_ssfr_small_scale = scale / (np.sqrt(cen_no_gals)* np.log(10.)*tukey)
+
+tukey, scale = tukey_biweight(cen_gas_h1)
+cen_h1_tukey = np.log10(tukey)
+cen_h1_large_scale = scale / (np.log(10.)*tukey)
+cen_h1_small_scale = scale / (np.sqrt(cen_no_gals)* np.log(10.)*tukey)
+
+cen_gas_fmol = cen_gas_h2 / cen_gas_h1
+tukey, scale = tukey_biweight(cen_gas_fmol)
+tukey[np.where(tukey == 0.)[0]] = 1.e-6
+cen_fmol_tukey = np.log10(tukey)
+cen_fmol_large_scale = scale / (np.log(10.)*tukey)
+cen_fmol_small_scale = (np.sqrt(cen_no_gals)* np.log(10.)*tukey)
+
+
+# satellites:
+sat_no_gals = len(sat_gas_sfr)
+
+tukey, scale = tukey_biweight(sat_gas_sfr)
+tukey[np.where(tukey == 0.)[0]] = 1.e-6
+sat_sfr_tukey = np.log10(tukey)
+sat_sfr_large_scale = scale / (np.log(10.)*tukey)
+sat_sfr_small_scale = scale / (np.sqrt(sat_no_gals)* np.log(10.)*tukey)
+
+sat_gas_ssfr = sat_gas_sfr / sat_star_m
+tukey, scale = tukey_biweight(sat_gas_ssfr)
+sat_ssfr_tukey = np.log10(tukey)
+sat_ssfr_large_scale = scale / (np.log(10.)*tukey)
+sat_ssfr_small_scale = scale / (np.sqrt(sat_no_gals)* np.log(10.)*tukey)
+
+tukey, scale = tukey_biweight(sat_gas_h1)
+sat_h1_tukey = np.log10(tukey)
+sat_h1_large_scale = scale / (np.log(10.)*tukey)
+sat_h1_small_scale = scale / (np.sqrt(cen_no_gals)* np.log(10.)*tukey)
+
+sat_gas_fmol = sat_gas_h2 / sat_gas_h1
+tukey, scale = tukey_biweight(sat_gas_fmol)
+tukey[np.where(tukey == 0.)[0]] = 1.e-6
+sat_fmol_tukey = np.log10(tukey)
+sat_fmol_large_scale = scale / (np.log(10.)*tukey)
+sat_fmol_small_scale = scale / (np.sqrt(sat_no_gals)* np.log(10.)*tukey)
+
+with h5py.File(results_dir+selection+'_ssfr_data.h5', 'a') as f:
+        f.create_dataset('cen_no_gals_'+bin_label, data=np.array(cen_no_gals))
+        f.create_dataset('sat_no_gals_'+bin_label, data=np.array(sat_no_gals))
+        f.create_dataset('cen_tukey_'+bin_label, data=np.array(cen_ssfr_tukey))
+        f.create_dataset('sat_tukey_'+bin_label, data=np.array(sat_ssfr_tukey))
+        f.create_dataset('cen_large_scale_'+bin_label, data=np.array(cen_ssfr_large_scale))
+        f.create_dataset('sat_large_scale_'+bin_label, data=np.array(sat_ssfr_large_scale))
+        f.create_dataset('cen_small_scale_'+bin_label, data=np.array(cen_ssfr_small_scale))
+        f.create_dataset('sat_small_scale_'+bin_label, data=np.array(sat_ssfr_small_scale))
+
+with h5py.File(results_dir+selection+'_sfr_data.h5', 'a') as f:
+        f.create_dataset('cen_no_gals_'+bin_label, data=np.array(cen_no_gals))
+        f.create_dataset('sat_no_gals_'+bin_label, data=np.array(sat_no_gals))
+        f.create_dataset('cen_tukey_'+bin_label, data=np.array(cen_sfr_tukey))
+        f.create_dataset('sat_tukey_'+bin_label, data=np.array(sat_sfr_tukey))
+        f.create_dataset('cen_large_scale_'+bin_label, data=np.array(cen_sfr_large_scale))
+        f.create_dataset('sat_large_scale_'+bin_label, data=np.array(sat_sfr_large_scale))
+        f.create_dataset('cen_small_scale_'+bin_label, data=np.array(cen_sfr_small_scale))
+        f.create_dataset('sat_small_scale_'+bin_label, data=np.array(sat_sfr_small_scale))
+
+with h5py.File(results_dir+selection+'_h1_data.h5', 'a') as f:
+        f.create_dataset('cen_no_gals_'+bin_label, data=np.array(cen_no_gals))
+        f.create_dataset('sat_no_gals_'+bin_label, data=np.array(sat_no_gals))
+        f.create_dataset('cen_tukey_'+bin_label, data=np.array(cen_h1_tukey))
+        f.create_dataset('sat_tukey_'+bin_label, data=np.array(sat_h1_tukey))
+        f.create_dataset('cen_large_scale_'+bin_label, data=np.array(cen_h1_large_scale))
+        f.create_dataset('sat_large_scale_'+bin_label, data=np.array(sat_h1_large_scale))
+        f.create_dataset('cen_small_scale_'+bin_label, data=np.array(cen_h1_small_scale))
+        f.create_dataset('sat_small_scale_'+bin_label, data=np.array(sat_h1_small_scale))
+
+with h5py.File(results_dir+selection+'_fmol_data.h5', 'a') as f:
+        f.create_dataset('cen_no_gals_'+bin_label, data=np.array(cen_no_gals))
+        f.create_dataset('sat_no_gals_'+bin_label, data=np.array(sat_no_gals))
+        f.create_dataset('cen_tukey_'+bin_label, data=np.array(cen_fmol_tukey))
+        f.create_dataset('sat_tukey_'+bin_label, data=np.array(sat_fmol_tukey))
+        f.create_dataset('cen_large_scale_'+bin_label, data=np.array(cen_fmol_large_scale))
+        f.create_dataset('sat_large_scale_'+bin_label, data=np.array(sat_fmol_large_scale))
+        f.create_dataset('cen_small_scale_'+bin_label, data=np.array(cen_fmol_small_scale))
+        f.create_dataset('sat_small_scale_'+bin_label, data=np.array(sat_fmol_small_scale))
 
