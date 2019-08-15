126fd3ae33e338778ab00061e8bb864317fc32db
Sarah Appleby <sarahappleby20@gmail.com>
2019-08-15 14:31:55 +0000

    limits on axes

--- profiles/paper/plotting/plot_radial_tukeys.py
+++ profiles/paper/plotting/plot_radial_tukeys.py
@@ -11,25 +11,26 @@ plt.rcParams.update({'font.size': 14})
 
 cmap = cm.get_cmap('plasma')
 colors_b18 = [cmap(0.85), cmap(0.65), cmap(0.5), cmap(0.3)]
+colors_b18 = [cmap(0.8), cmap(0.6), cmap(0.3)]
 
 cmap = cm.get_cmap('viridis')
 colors = [cmap(0.85), cmap(0.6), cmap(0.35), cmap(0.15)]
+colors = [cmap(0.8), cmap(0.6), cmap(0.3)]
 
 #colors = ['g', 'c', 'b', 'm']
-selection = 'sf'
-gals = 'all'
-mass_b18 = [9.5, 10.0,  10.5, 11., 11.5]
+selection = 'gv'
+gals = 'cen'
+mass_b18 = [10.0,  10.5, 11., 11.5]
 
 data_dir = '/home/sapple/simba_sizes/profiles/paper/plotting/data/'
 plot_dir = '/home/sapple/simba_sizes/profiles/paper/plotting/plots/'
-bin_labels = ['9.5-10.0', '10.0-10.5', '10.5-11.0', '>11.0']
-plot_labels = [r'$9.5 < \textrm{log} (M_* / M_{\odot}) < 10.0$',
-                r'$10.0 < \textrm{log} (M_* / M_{\odot}) < 10.5$', 
+bin_labels = ['10.0-10.5', '10.5-11.0', '>11.0']
+plot_labels = [r'$10.0 < \textrm{log} (M_* / M_{\odot}) < 10.5$', 
                 r'$10.5 < \textrm{log} (M_* / M_{\odot}) < 11.0$', 
                 r'$ \textrm{log} (M_* / M_{\odot}) > 11.0$']
 xlabel = r'$ R / R_{half}$'
 
-fig, ax = plt.subplots(2, 2, figsize=(15, 15))
+fig, ax = plt.subplots(2, 2, figsize=(13, 13))
 ax = ax.flatten()
 
 # plot SFR:
@@ -46,12 +47,12 @@ for i, b in enumerate(bin_labels):
         bins = np.arange(0., factor, dr)
         rplot = bins+(dr*0.5)
 
-    ax[0].plot(rplot, tukey, color=colors[i], marker='.', markersize=5, linestyle='-', label=plot_labels[i]+'; '+str(int(no_gals))+' galaxies')
+    ax[0].plot(rplot, tukey, color=colors[i], linestyle='--', label=plot_labels[i]+'; '+str(int(no_gals))+' galaxies')
     if i == 1:
         ax[0].fill_between(rplot, tukey - large, tukey + large, color=colors[i], alpha=0.1)
     ax[0].fill_between(rplot, tukey - small, tukey + small, color=colors[i], alpha=0.3)
 
-ax[0].set_ylim(-5, )
+ax[0].set_ylim(-5, -1)
 if gals == 'all':
     ax[0].set_xlim(0, 3.5)
 else:
@@ -59,8 +60,9 @@ else:
 
 ax[0].set_xlabel(xlabel)
 ax[0].set_ylabel(r'$ \textrm{log} (\Sigma_{\textrm{SFR}} / M_{\odot}\textrm{yr}^{-1} \textrm{kpc}^{-2})$')
-if gals == 'all':
-    ax[0].legend(fontsize=12)
+if not gals == 'all':
+    ax[0].legend(fontsize=12, loc=1)
+
 
 plot_belfiore(ax[1], selection, colors_b18, mass_b18=mass_b18, label=True)
 # plot sSFR:
@@ -77,7 +79,7 @@ for i, b in enumerate(bin_labels):
         bins = np.arange(0., factor, dr)
         rplot = bins+(dr*0.5)
 
-    ax[1].plot(rplot, tukey, color=colors[i], marker='.', markersize=5, linestyle='-')
+    ax[1].plot(rplot, tukey, color=colors[i], linestyle='--')
     if i == 1:
         ax[1].fill_between(rplot, tukey - large, tukey + large, color=colors[i], alpha=0.1)
     ax[1].fill_between(rplot, tukey - small, tukey + small, color=colors[i], alpha=0.3)
@@ -86,10 +88,13 @@ if gals == 'all':
     ax[1].set_xlim(0, 3.5)
 else:
     ax[1].set_xlim(0., 2)
-ax[1].set_ylim(-12.5, -9.0)
+ax[1].set_ylim(-12.5, -9.5)
 ax[1].set_xlabel(xlabel)
 ax[1].set_ylabel(r'$\textrm{log} (\textrm{sSFR} / \textrm{yr}^{-1})$')
-ax[1].legend(fontsize=12)
+if not gals == 'all':
+    ax[1].legend(fontsize=12, loc=1)
+else:
+    ax[1].legend(fontsize=12, loc=3)
 
 # plot HI:
 for i, b in enumerate(bin_labels):
@@ -105,7 +110,7 @@ for i, b in enumerate(bin_labels):
         bins = np.arange(0., factor, dr)
         rplot = bins+(dr*0.5)
 
-    ax[2].plot(rplot, tukey, color=colors[i], marker='.', markersize=5, linestyle='-', label=plot_labels[i]+'; '+str(int(no_gals))+' galaxies')
+    ax[2].plot(rplot, tukey, color=colors[i], linestyle='--', label=plot_labels[i]+'; '+str(int(no_gals))+' galaxies')
     if i == 1:
         ax[2].fill_between(rplot, tukey - large, tukey + large, color=colors[i], alpha=0.1)
     ax[2].fill_between(rplot, tukey - small, tukey + small, color=colors[i], alpha=0.3)
@@ -113,10 +118,11 @@ for i, b in enumerate(bin_labels):
 if not gals == 'all':
     ax[2].set_ylim(3.5, )
 ax[2].set_xlim(0, 5)
+ax[2].set_ylim(4.5,8.)
 ax[2].set_xlabel(xlabel)
 ax[2].set_ylabel(r'$ \textrm{log} (\Sigma_{HI} / M_{\odot}\textrm{kpc}^{-2})$')
-if not gals == 'all':
-    ax[2].legend(fontsize=12)
+if gals == 'all':
+    ax[2].legend(fontsize=12, loc=3)
 
 
 
@@ -134,7 +140,7 @@ for i, b in enumerate(bin_labels):
         bins = np.arange(0., factor, dr)
         rplot = bins+(dr*0.5)
 
-    ax[3].plot(rplot, tukey, color=colors[i], marker='.', markersize=5, linestyle='-')
+    ax[3].plot(rplot, tukey, color=colors[i], linestyle='--')
     if i == 1:
         ax[3].fill_between(rplot, tukey - large, tukey + large, color=colors[i], alpha=0.1)
     ax[3].fill_between(rplot, tukey - small, tukey + small, color=colors[i], alpha=0.3)
