b5ccdf40ad5778ee8e97753afa7d11712f9cac0e
Sarah Appleby <sarahappleby20@gmail.com>
2019-08-20 12:29:12 +0000

    add sdss points at z = 0

--- sizes/van_der_wel_redshift.py
+++ sizes/van_der_wel_redshift.py
@@ -1,6 +1,4 @@
 import matplotlib
-#matplotlib.rcParams['text.usetex'] = True
-#matplotlib.rcParams['text.latex.unicode'] = True
 import matplotlib.pyplot as plt
 import os
 import numpy as np
@@ -13,12 +11,12 @@ plt.rc('font', family='serif')
 def evolution(z, a, b):
     return a*((1+z)**b)
 
-radius_file = '/home/sapple/simba_sizes/sizes/data/halfradius.h5'
+radius_file = '/home/sapple/simba_sizes/sizes/data/halfradius_V.h5'
 plot_dir = '/home/sapple/simba_sizes/sizes/plots/'
 simba_caesar = '/home/sapple/simba_sizes/sizes/data/'
 
 model = 'm100n1024'
-wind = 's50j7k'
+wind = 's50'
 
 m_star = 5.e10
 m_star_str = str(m_star)[0] + 'e' + str(int(np.log10(m_star)))
@@ -37,7 +35,6 @@ red_sizes = np.zeros(len(simba_z))
 red_per_25 = np.zeros(len(simba_z))
 red_per_75 = np.zeros(len(simba_z))
 
-i = 0
 for i, snap in enumerate(simba_snaps):
     with h5py.File(radius_file, 'r') as r:
         rhalf = r[model+'_'+wind+'_'+snap+'_halflight'][:]
@@ -63,7 +60,7 @@ for i, snap in enumerate(simba_snaps):
     ssfr = np.log10(1.e9*sfr/smass)
     ssfrlim = -1.8+0.3*simba_z[i]
 
-    mask = (smass > 0.95*m_star) & (smass < 1.05*m_star)
+    mask = (smass > 0.9*m_star) & (smass < 1.1*m_star)
     star_forming = ssfr > ssfrlim
 
     blue_sizes[i] = np.median(rhalf[central*mask*star_forming])
@@ -74,41 +71,36 @@ for i, snap in enumerate(simba_snaps):
     red_per_25[i] = np.percentile(rhalf[central*mask*np.invert(star_forming)], 25)
     red_per_75[i] = np.percentile(rhalf[central*mask*np.invert(star_forming)], 75)
 
-# plotting the fits with the data
+    print 'z = ' + str(simba_z[i])
+    print 'Number of star forming galaxies: ' + str(len(rhalf[central*mask*star_forming]))
+    print 'Number of passive galaxies: ' + str(len(rhalf[central*mask*np.invert(star_forming)]))
+    print '\n'
+
 popt, pcov = curve_fit(evolution, simba_z, blue_sizes)
 blue_string = r'$R/\textrm{kpc} = %.1f (1 + z)^{%.2f}$' % (popt[0], popt[1])
 blue_fit = evolution(np.array(simba_z), popt[0], popt[1])
-plt.plot(simba_z, blue_fit, linestyle='--', c='c', lw=1.5, label='Star forming; '+ blue_string)
 
 popt, pcov = curve_fit(evolution, simba_z, red_sizes)
 red_string = r'$R/\textrm{kpc} = %.1f (1 + z)^{%.2f}$' % (popt[0], popt[1])
 red_fit = evolution(np.array(simba_z), popt[0], popt[1])
-plt.plot(simba_z, red_fit, linestyle='--', c='m', lw=1.5, label='Passive; '+ red_string)
 
-plt.plot(np.arange(0., 3.1, 0.1), vdw_blue, '-', lw=1.5, color='b', label='CANDELS-LTG (van der Wel+14)')
-plt.plot(np.arange(0., 3.1, 0.1), vdw_red, '-', lw=1.5, color='r', label='CANDELS-ETG (van der Wel+14)')
-
-plt.xlabel(r'z', fontsize=16)
-#plt.xlim(3.1, -0.1)
-plt.ylabel(r'$R_\textrm{half}\ (\textrm{kpc})$' ,fontsize=16)
-plt.legend(loc=1)
-plt.savefig(plot_dir+'redshift_fits_'+m_star_str+'.png')
-plt.clf()
-
+plt.plot(0., 0.81, color='b', marker='s', markersize=5., linestyle='None', label='SDSS-SF')
+plt.plot(0., 0.69, color='r', marker='s', markersize=5., linestyle='None', label='SDSS-Q')
 
 # plotting the medians with the data
-plt.plot(np.arange(0., 3.1, 0.1), vdw_blue, '-', lw=1.5, color='b', label='CANDELS-LTG (van der Wel+14)')
-plt.plot(np.arange(0., 3.1, 0.1), vdw_red, '-', lw=1.5, color='r', label='CANDELS-ETG (van der Wel+14)')
+plt.plot(np.arange(0., 3.1, 0.1), np.log10(vdw_blue), '-', lw=1.5, color='b', label='CANDELS-LTG (van der Wel+14)')
+plt.plot(np.arange(0., 3.1, 0.1), np.log10(vdw_red), '-', lw=1.5, color='r', label='CANDELS-ETG (van der Wel+14)')
 
-plt.plot(simba_z, blue_sizes, linestyle='--', marker='o', markersize=5, c='c', lw=1.5, label='Star forming; '+ blue_string)
-plt.fill_between(simba_z, blue_per_25, blue_per_75, facecolor='c', alpha=0.15, linewidth=1)
+plt.plot(simba_z, np.log10(blue_sizes), linestyle='--', c='c', lw=1.5, label='Star forming; '+ blue_string)
+plt.fill_between(simba_z, np.log10(blue_per_25), np.log10(blue_per_75), facecolor='c', alpha=0.15, linewidth=1)
 
-plt.plot(simba_z, red_sizes, linestyle='--', marker='o', markersize=5, c='m', lw=1.5, label='Passive; '+red_string)
-plt.fill_between(simba_z, red_per_25, red_per_75, facecolor='m', alpha=0.15, linewidth=1)
+plt.plot(simba_z, np.log10(red_sizes), linestyle='--', c='m', lw=1.5, label='Passive; '+red_string)
+plt.fill_between(simba_z, np.log10(red_per_25), np.log10(red_per_75), facecolor='m', alpha=0.15, linewidth=1)
 
 plt.xlabel(r'z', fontsize=16)
 #plt.xlim(3.1, -0.1)
-plt.ylabel(r'$R_\textrm{half}\ (\textrm{kpc})$' ,fontsize=16)
+plt.ylabel(r'$\textrm{log}\ ( R_\textrm{half}\ /\ \textrm{kpc})$' ,fontsize=16)
 plt.legend(loc=1)
 plt.savefig(plot_dir+'redshift_medians_'+m_star_str+'.png')
+plt.show()
 plt.clf()
